43cd14005a4dc55ac6a1bfdc228964bbe3f57830
aiiiden0 <hjjam100@gmail.com>
2025-01-11 09:55:29 +0900

    chore: update url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,11 +13,10 @@ const config: Config = {
   favicon: 'img/favicon.svg',
 
   // Set the production url of your site here
-  url: 'https://iteratefast-labs.github.io',
+  url: 'https://docs.fission.lol',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/fission',
-  // baseUrl: '',
+  baseUrl: '',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
